1df0cdbd38cd66118a9f3dd6d28f5adae1a40ded
luli <luli@tsign.cn>
2021-11-03 18:09:55 +0800

    fix

---
+++ basic/cal.py
@@ -0,0 +1,4 @@
+1 + 1
+print(5 ** 2)
+print(5 // 2)
+print("fksjfklsj")
---
+++ basic/string.py
@@ -0,0 +1,27 @@
+print('"Isn\'t," they said.')
+print(r'C:\some\name') 
+print(3 * 'xxuu' + "fksdjf")
+print('aaa' 'bbb')
+text=('abcdefg'
+    'kjfslj')
+print(text)
+
+print(3 * 'uuu' + 'kjfsklj')
+
+print('abcde'[0])
+print('abcde'[-0])
+print('abcde'[-1])
+
+print('abcde'[1:3])
+print('abcde'[-2:])
+print('abcde'[:4])
+
+a='abcde时间'
+print(a[:4] + a[4:])
+
+print(len(a))
+
+print(f'{a}kfjsldfj')
+print(f"""xxx""""fksdj")
+
+# a[1] = '2' # error
