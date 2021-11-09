8e3f75efdff1923cdaeb034f136022f74252f292
luli <luli@tsign.cn>
2021-11-09 14:01:06 +0800

    fix

---
+++ tuple.py
@@ -0,0 +1,21 @@
+t = 1, 2, 'aa', 'b', ('a','b','c','d')
+print(t[4][2])
+
+l = [1,2,3,4]
+
+"""
+tuples can not be changed while list can, but list occupy more mem space than tuple
+"""
+
+print(dir(t))
+print(80*'-')
+print(dir(l))
+
+import sys
+print(dir(sys))
+# print(help(sys.getsizeof))
+
+d1 = [1,2,3,4]
+d2 = 1,2,3,4
+print(sys.getsizeof(d1))
+print(sys.getsizeof(d2))
