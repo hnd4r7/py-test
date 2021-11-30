71e2bca10f8c5caaf5b5d68e18e85f6b11e02690
luli <luli@tsign.cn>
2021-11-30 11:58:45 +0800

    fix

--- for.py
+++ for.py
@@ -72,3 +72,59 @@ for value in raw_data:
         filtered_data.append(value)
 
 print(filtered_data)
+
+'''
+Behind the scenes, the for statement calls iter() on the container object. 
+The function returns an iterator object that defines the method __next__() which accesses elements in the container one at a time.
+When there are no more elements, __next__() raises a StopIteration exception which tells the for loop to terminate. 
+You can call the __next__() method using the next() built-in function; this example shows how it all works:
+'''
+
+s = 'abc'
+it = iter(s)
+print(next(it))
+print(next(it))
+print(next(it))
+
+class Reverse:
+    """Iterator for looping over a sequence backwards."""
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.data[self.index]
+
+rev = Reverse('spam')
+for char in rev:
+    print(char)
+
+print('-----'*10)
+
+def reverse(data):
+    for index in range(len(data)-1, -1, -1):
+        yield data[index]
+
+for char in reverse('golf'):
+    print(char)
+
+
+# A Simple Python program to demonstrate working
+# of yield
+  
+# A generator function that yields 1 for the first time,
+# 2 second time and 3 third time
+def simpleGeneratorFun():
+    yield 1
+    yield 2
+    yield 3
+  
+# Driver code to check above generator function
+for value in simpleGeneratorFun(): 
+    print(value)
